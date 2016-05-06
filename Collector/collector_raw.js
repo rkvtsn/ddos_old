cee7f1b9fb1c9aef553de990a83e58eaa0ea20f6
Andrey <Andrey>
2016-05-06 18:41:03 +0300

    ~

--- Collector/collector_raw.js
+++ Collector/collector_raw.js
@@ -120,9 +120,11 @@ if (cluster.isMaster) {
                 var info = nfc.inline(msg.tm);
 
                 storage.store({ time: new Date(msg.tm), buffer: nfc.bytesArray, atk_name: attack['name'], atk_desc: attack['desc'] }, function () {
-                    socket_s.send(info);
+                    
                 });
                 
+                socket_s.send(info);
+
                 console.log('Stored: ' + nfc.bytesArray.length);
 
                 var interval = config.interval / 1000;
