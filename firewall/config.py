48ff5715e37dd255325b16a1ac91c243c44efd02
Andrey <Andrey>
2016-06-06 16:40:28 +0300

    ~

--- firewall/config.py
+++ firewall/config.py
@@ -4,7 +4,7 @@ dir = "firewall"
 db_name = "rules.sqlite"
 backup_name = "bak.iptables"
 
-db_path = path.join(dir, db)
+db_path = path.join(dir, db_name)
 backup_path = path.join(dir, backup_name)
 
 # timeout for Firewall.Warden seconds
